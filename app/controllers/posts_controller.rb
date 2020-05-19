cd203abf5e4f3665b72f0ca455e89434df511e3b
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-05-19 12:34:08 +0900

    fix side menu

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,6 @@ class PostsController < ApplicationController
 # 削除ではgem data-confirm-modalを使用すること
 
   def index
-    @usercontents = ["マイメニュー", "マイグループ", "投稿"]
-    @sidecontents = ["新着情報", "お悩み相談室(ChatSpace風)", "グループ一覧", "バイク板", "おすすめルート", "おすすめスポット", "おすすめのお店", "おすすめの施設", "おすすめのウェア", "おすすめのアイテム", "交通情報の共有", "お問い合わせ"]
   end
   
 end
