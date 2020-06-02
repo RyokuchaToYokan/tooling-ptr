b8387f1143109de6583aa63679004f1a8cc77733
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-06-02 14:27:24 +0900

    edit  Q & A room

--- app/assets/javascripts/accordionmenu.js
+++ app/assets/javascripts/accordionmenu.js
@@ -1,9 +1,24 @@
-$(function() {
-  //.side__head__box__menu__iconがクリックされた時
-  $('.side__head__box__menu__icon').click(function() {
-      //クリックされた.side__head__box__menu__iconの次の要素の横幅を開いたり閉じたりする。
-      $(this).next().slideToggle();
-      //クリックされた.side__head__box__menu__iconの次の要素の横幅を閉じる。
-      $('.side__head__box__menu__icon').not($(this)).slideToggle();
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    //.side__head__box__menu__iconがクリックされた時
+    $('.side__head__box__menu__icon').click(function() {
+        //クリックされた.side__head__box__menu__iconの次の要素の横幅を開いたり閉じたりする。
+        $(this).next().slideToggle();
+        //クリックされた.side__head__box__menu__iconの次の要素の横幅を閉じる。
+        $('.side__head__box__menu__icon').not($(this)).slideToggle();
+    });
   });
+
+  $(function () {
+    // ナビの範囲外のどこかをクリックしたときに発動
+    $(".main").on('click', function () {
+        if ($('.side__head__box__menu__icon').is(':visible')) {
+            // ナビが表示されていたらcloseを実行
+            $('.side__head__box__menu__icon').trigger('click');
+        } else {
+            // ナビが非表示の場合は起動しない
+            event.stopPropagation();
+        }
+    });
+});
 });
