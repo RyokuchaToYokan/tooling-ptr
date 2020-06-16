70d45f3e5a9b6b1b928d45fa8e7ab93a14d1f022
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-06-16 23:31:53 +0900

    halftime next chat scrol

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,16 @@ class Room < ApplicationRecord
   validates :name, presence: true
 
   attribute :status_id, :integer, default: 1
+
+  def show_last_chat
+    if (last_chat = chats.last).present?
+      if last_chat.content?
+        last_chat.content
+      else
+        "画像が投稿されています"
+      end
+    else
+      "まだメッセージはありません"
+    end
+  end
 end
