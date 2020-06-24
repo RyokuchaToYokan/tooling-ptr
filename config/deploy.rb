cc6ad35a8e300feb10d90fcf6d06d0795f357e80
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-06-25 04:54:14 +0900

    fix for curriculum deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,34 @@
-# Gemfile.lockを見てcapistranoのバージョンを入れる
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.1'
 
-# 自身のアプリ名、リポジトリ名を記述
+# Capistranoのログの表示に利用する
 set :application, 'tooling-ptr'
+
+# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:RyokuchaToYokan/tooling-ptr.git'
 
+# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
-# pemを指定
+# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tooling-ptr.pem']
+                  keys: ['~/.ssh/tooling-ptr.pem'] 
 
+# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/master.key }
-
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
-
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
