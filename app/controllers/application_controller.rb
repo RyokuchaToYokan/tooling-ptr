644402454b112a60d88c0e78c60bf8855a429e43
RyokuchaToYokan <autumnalbrook@gmail.com>
2020-06-25 05:54:20 +0900

    add basic_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth, if: :production?
   # before_action :authenticate_user!     #ログイン必須機能
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -20,6 +21,20 @@ class ApplicationController < ActionController::Base
 
   def set_history
     @historys = current_user.rooms.order("created_at DESC") if user_signed_in?
+  end
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
+
+  def production?
+    Rails.env.production?
   end
   
 end
